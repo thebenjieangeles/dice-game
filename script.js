ea60a520711484c611a2c82c073507a09cabe379
thebenjieangeles <thebenjangeles@gmail.com>
2025-08-10 19:58:41 +0800

    fixed bug header now clickable

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-// Function to roll the dice
 function rollDice() {
-  // Roll first dice
   let randomNumber1 = Math.floor(Math.random() * 6) + 1;
   let imageName1 = "dice" + randomNumber1 + ".png";
   document
     .querySelectorAll("img")[0]
     .setAttribute("src", "./images/" + imageName1);
 
-  // Roll second dice
   let randomNumber2 = Math.floor(Math.random() * 6) + 1;
   let imageName2 = "dice" + randomNumber2 + ".png";
   document
     .querySelectorAll("img")[1]
     .setAttribute("src", "./images/" + imageName2);
 
-  // Change heading based on results
   let heading = document.querySelector("h1");
 
   if (randomNumber1 > randomNumber2) {
@@ -26,5 +22,4 @@ function rollDice() {
   }
 }
 
-// Run rollDice when h1 is clicked
 document.querySelector("h1").addEventListener("click", rollDice);
